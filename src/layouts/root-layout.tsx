b331b6906e287d3d9c48d6e4c26eba5a512e37fd
Huynh Huy <huynhhuy.forwork@gmail.com>
2026-05-16 18:49:50 +0700

    feat: update proper padding

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -58,7 +58,7 @@ const RootLayout = () => {
         ref={scrollRef}
       >
         <div
-          style={{ paddingBottom: `${navigationBarHeight + 50}px` }}
+          style={{ paddingBottom: `${navigationBarHeight + 60}px` }}
           className="flex flex-col items-center px-5 pt-5 relative w-full"
         >
           <Outlet />
